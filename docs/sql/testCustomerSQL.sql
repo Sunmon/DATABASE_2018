17657936ed2b4e2fb76b377f64f41679cf67071b
sunmon <blackmilk274@gmail.com>
2018-11-14 15:55:39 +0900

    update : clear project folder
    프로젝트 폴더를 정리했습니다

---
+++ docs/sql/testCustomerSQL.sql
@@ -0,0 +1,10 @@
+
+-- create account
+INSERT INTO person (ID, password, name, age, gender, phone, address, height, weight, authority) 
+values("sunmon", "1234", "sunjung", 23, "F", "010-4543-5364", "songpa-gu", 168, 0, "customer");
+
+
+select *
+from person;
+
+
