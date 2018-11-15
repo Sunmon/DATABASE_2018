60de2b966cd8e8eee3ae6c21c283fc41c6b69f0e
sunmon <blackmilk274@gmail.com>
2018-11-15 12:31:30 +0900

    create: add sql without p_type
    p_type테이블이 사라진 데이터베이스의 sql을 추가했다.
    create / foreign 둘 다 돌아간다.

--- docs/sql/testCustomerSQL.sql
+++ docs/sql/testCustomerSQL.sql
@@ -46,16 +46,105 @@ FROM cart
 
 -- 7. add favorites	//java
 INSERT INTO favorites
-values("AA-121", "sunmon2", "eiwh");
+values("AAdd-11", "sunmon2", "seller_1");
 
 
+-- 7.1 add favorites	//java
+INSERT INTO favorites
+SELECT p_code, "sunmon", seller_id
+FROM sell_list
+WHERE p_nickname = "cute t-shirt"; 
+
+
+
+//TODO: 여기
+-- 8. remove from favorites	//java
+#DELETE FROM favorites
+#where p_code = 
+
+select *
+from favorites;
+
+select *
+from sell_list;
 
 
 select *
-from person, category, type, product, log ;
+from person, category, p_type, product, sell_log ;
 
 select *
 from category;
 
+
+select *
+from product;
+
+
+select *
+from p_type;
+
+
+
+-- for testing category ... 13
 INSERT INTO category
-values ("AA", "bb", 
+values ("AA", "bb", "tutle");
+
+INSERT INTO category
+values ("AA", "cc", "round");
+
+INSERT INTO category
+values ("BB", "aa", "jacket");
+
+INSERT INTO category
+values ("BB", "dd", "coat");
+
+
+-- for testing product ...
+INSERT INTO product
+values ("KONKUK", "Gwa-Jam");
+
+INSERT INTO product
+values ("AAbb-12", "nice round tutleneck");
+
+INSERT INTO product
+values ("AAdd-11", "awesome shirt");
+
+
+-- for testing p_type
+INSERT INTO p_type
+values("AAdd-11", "AA", "bb");
+
+INSERT INTO p_type
+values("KONKUK", "BB", "aa");
+
+INSERT INTO p_type
+values ("AAbb-12", "AA", "bb");
+
+select *
+from product;
+
+select *
+
+select *
+from p_type;
+
+-- for testing seller
+-- create account
+INSERT INTO person (ID, pw, name, age, gender, phone, address, height, weight) 
+values("seller_1", "1234", "seller1", 23, "F", "010-1231-1111", "songpa-gu", 168, 100);
+
+-- modify authority user to sellers
+UPDATE person
+SET authority = "sellers"
+where id = "seller_1";
+
+
+-- for testing sell_list
+INSERT INTO sell_list
+values("AAdd-11", "seller_1", 0, 1, "S", "cute t-shirt");
+
+
+select *
+from sell_list;
+select *
+from person;
