d320dceeeda033bc77b80d6c0da630ffbf6a3ccc
leesewoong <44529918+tomy807@users.noreply.github.com>
2018-12-02 05:34:29 +0900

    delete추가

--- src/view/VMypage.java
+++ src/view/VMypage.java
@@ -4,24 +4,22 @@ import model.User;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.sql.SQLException;
 
-public class VMypage extends JPanel
-{
+public class VMypage extends JPanel {
     private JPanel mainPanel;
     private JScrollPane tablePanel;
     private JTable mTable;
     private JButton applyButton;
     private JButton pointButton;
 
-    public VMypage()
-    {
+    public VMypage() {
         add(mainPanel);
         setVisible(true);
     }
 
     //...JTable(view)에 띄울 데이터 설정
-    public void initTable(User user, Connector con)
-    {
+    public void initTable(User user, Connector con) {
         //Column 설정
         String col[] = {" ", " "};
         Object[][] data = {{"ID", user.getID()},    //0
@@ -37,15 +35,12 @@ public class VMypage extends JPanel
                 {"TYPE", user.getAuthority()}};     //10
 
         //DefaultTableModel 생성
-        DefaultTableModel dtm = new DefaultTableModel(data, col)
-        {
+        DefaultTableModel dtm = new DefaultTableModel(data, col) {
             //사용자가 수정할 수 있는 부분, 없는 부분 설정
             @Override
-            public boolean isCellEditable(int row, int col)
-            {
+            public boolean isCellEditable(int row, int col) {
                 if (col == 0) return false; //테이블 정보는 수정불가
-                switch (row)
-                {
+                switch (row) {
                     case 1:
                     case 3:
                     case 4:
@@ -68,7 +63,18 @@ public class VMypage extends JPanel
     //FIXME: 내가 구매한 목록 기록 보기
 
 
-
     //TODO: 수정 버튼 이벤트
-    //TODO: 포인트 추가 버튼 이벤트
+    public void applychanges(User user, Connector con) {
+
+
+            String pn = (String) mTable.getValueAt(1, 1);
+            int age = (int) mTable.getValueAt(3, 1);
+            String gen = (String) mTable.getValueAt(4, 1);
+            String adr = (String) mTable.getValueAt(6, 1);
+            int hei = (int) mTable.getValueAt(7, 1);
+            int wei = (int) mTable.getValueAt(8, 1);
+            con.updateMypage(pn, age, gen, adr, hei, wei, user.getID(), user.getPhone());
+
+    }
 }
+    //TODO: 포인트 추가 버튼 이벤트
